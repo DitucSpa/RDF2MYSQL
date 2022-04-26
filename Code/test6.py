dcf7d45e9a86cd65afcf040a89572fb0210da256
Gianluca Di Tuccio <gianluca.dituccio@outlook.it>
2022-04-26 18:15:26 +0200

    26-04-2022

--- Code/test6.py
+++
@@ -1,7 +0,0 @@
-# pip install python-magic-bin==0.4.14
-import magic
-
-print(magic.from_file('jsonld.owl'))
-
-# printing the mime type of the file
-print(magic.from_file('jsonld.owl', mime = True))
